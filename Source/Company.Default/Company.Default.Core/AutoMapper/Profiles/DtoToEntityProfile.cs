8d6f5b45b5101b404cb76ad0ec4e0bb7e8a8ec27
Angélica Flausino <alpha2office@gmail.com>
2023-01-11 09:17:24 -0300

    adjust automapper

---
+++ Source/Company.Default/Company.Default.Core/AutoMapper/Profiles/DtoToEntityProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Company.Default.Domain.Dtos;
+using Company.Default.Domain.Entities;
+
+namespace Company.Default.Core.AutoMapper.Profiles
+{
+    public class DtoToEntityProfile : Profile
+    {
+        public DtoToEntityProfile()
+        {
+            CreateMap<PersonDto, Person>()
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.GetAge()));
+        }
+    }
+}
--- Source/Company.Default/Company.Default.Core/CoreConfigurationExtensions.cs
+++ Source/Company.Default/Company.Default.Core/CoreConfigurationExtensions.cs
@@ -15,6 +15,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddAutoMapper(cfg =>
             {
                 cfg.AddProfile<EntityToDtoProfile>();
+                cfg.AddProfile<DtoToEntityProfile>();
             });
 
             //Validations
