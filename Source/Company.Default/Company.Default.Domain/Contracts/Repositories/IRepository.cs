2e1edce4a79dee5f95b0e54ccd3d40a001bfeb14
Angélica Flausino <alpha2office@gmail.com>
2023-02-16 16:40:37 -0300

    add test repository in memory

--- Source/Company.Default/Company.Default.Domain/Contracts/Repositories/IRepository.cs
+++ Source/Company.Default/Company.Default.Domain/Contracts/Repositories/IRepository.cs
@@ -6,13 +6,13 @@ namespace Company.Default.Domain.Contracts.Repositories
     public interface IRepository<TEntity, TKey> where TEntity : class
     {
         void Add(TEntity entity);
-        Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
         void AddRange(IEnumerable<TEntity> entities);
-        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default );
         TEntity GetById(TKey keyId);
-        ValueTask<TEntity> GetByIdAsync(TKey keyId, CancellationToken cancellationToken);
+        ValueTask<TEntity> GetByIdAsync(TKey keyId, CancellationToken cancellationToken = default);
         TEntity FindById(TKey keyId);
-        ValueTask<TEntity> FindByIdAsync(TKey keyId, CancellationToken cancellationToken);
+        ValueTask<TEntity> FindByIdAsync(TKey keyId, CancellationToken cancellationToken = default);
         void Update(TEntity entity);
         void UpdateRange(IEnumerable<TEntity> entities);
         void Delete(TEntity entity);
