51992d6b643d0204c1d6666a0ed356a5dbfa3480
Angélica Flausino <alpha2office@gmail.com>
2023-02-28 10:58:51 -0300

    Updated ApiOptions to get values ​​from appsettings

--- Source/Company.Default/Company.Default.Domain/Options/ApiOptions.cs
+++ Source/Company.Default/Company.Default.Domain/Options/ApiOptions.cs
@@ -1,10 +1,29 @@
-﻿namespace Company.Default.Domain.Options
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Company.Default.Domain.Options
 {
-    //TODO: Obter valores do appsettings
     public static class ApiOptions
     {
-        public static int DefaultPage = 1;
-        public static int DefaultPageSize = 20;
-        public static string DefaultSortBy = "CreatedAt asc";
+        private static IConfiguration _config;
+        private static string _apiOptionsKey = "ApiOptions";
+        private static string _defaultPageKey = "DefaultPage";
+        private static string _defaultPageSizeKey = "DefaultPageSize";
+        private static string _defaultSortByKey = "DefaultSortBy";
+
+        public static int DefaultPage { get; private set; }
+        public static int DefaultPageSize { get; private set; }
+        public static string DefaultSortBy { get; private set; }
+
+        public static void ConfigureApiOptions(IConfiguration configuration)
+        {
+            if(configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            _config = configuration;
+
+            DefaultPage = _config.GetValue<int>($"{_apiOptionsKey}:{_defaultPageKey}");
+            DefaultPageSize = _config.GetValue<int>($"{_apiOptionsKey}:{_defaultPageSizeKey}");
+            DefaultSortBy = _config.GetValue<string>($"{_apiOptionsKey}:{_defaultSortByKey}");
+        }
     }
 }
